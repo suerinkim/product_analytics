7d646dc028d3498b3d8b1f5a925531a959da4bac
davorzegarac <58695536+davorzegarac@users.noreply.github.com>
2020-02-07 14:39:02 +0100

    Update monthly_active.sql
    This query is really hard for database to execute. I propose alternative that returns same results.

--- monthly_active.sql
+++ monthly_active.sql
@@ -1,16 +1,15 @@
-
--- DAU, Rolling-28 MAU, Rolling-7 WAU (code doesn't seem to be working properly, not sure why)
 WITH dau_table AS (
-  SELECT reporting_date:: DATE as dt, count(distinct uuid_hash) AS dau
+  SELECT reporting_date:: DATE as dt, 
+         count(distinct uuid_hash) AS dau
   FROM public.dish_activity
   WHERE reporting_date > '2019-12-31'
   GROUP BY 1
   )
-SELECT dt, dau,
-       (SELECT count(distinct uuid_hash)
-        FROM public.dish_activity
-        WHERE reporting_date:: DATE BETWEEN a.dt - 28 AND a.dt) AS mau,
-       (SELECT count(distinct uuid_hash)
-        FROM public.dish_activity
-        WHERE reporting_date:: DATE BETWEEN a.dt - 7 AND a.dt) AS wau
-FROM dau_table a
+SELECT a.dt, 
+       a.dau ,
+       count(distinct uuid_hash) as mau,
+       count(distinct case when reporting_date:: DATE BETWEEN a.dt - 7 AND a.dt then uuid_hash end) as wau
+        FROM public.dish_activity,
+             dau_table a
+        WHERE reporting_date:: DATE BETWEEN a.dt - 28 AND a.dt 
+        GROUP BY a.dt, a.dau 
